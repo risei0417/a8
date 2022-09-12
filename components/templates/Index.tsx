56b301599a082766126cbe1ba2398986023c3044
risei0417 <daiya5204156@outlook.jp>
2022-09-13 01:19:59 +0900

    #16 対応

--- components/templates/Index.tsx
+++ components/templates/Index.tsx
@@ -1,68 +1,29 @@
 import { motion, Variants } from "framer-motion";
-import { useEffect, useState } from "react";
-import { random } from "utils/math";
 import styles from "styles/Index.module.scss";
+import { Welcome } from "components/molecules/Welcome";
+import { useMemo } from "react";
+import { useRouter } from "next/dist/client/router";
 
 export const Index = () => {
-  const [colors, setColors] = useState<string[]>([]);
-
-  useEffect(() => {
-    const array = [];
-    for (let i = 0; i < 300; i++) {
-      array.push(
-        `rgb(${random(255, 0)}, ${random(255, 0)}, ${random(255, 0)})`
-      );
-    }
-    setColors(array);
-  }, []);
-
-  const cardVariants: Variants = {
-    offscreen: {
-      y: 300,
-    },
-    onscreen: {
-      y: 0,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 0.8,
+  const cardVariants: Variants = useMemo(() => {
+    return {
+      offscreen: {
+        y: 300,
       },
-    },
-  };
+      onscreen: {
+        y: 0,
+        transition: {
+          type: "spring",
+          bounce: 0.4,
+          duration: 0.8,
+        },
+      },
+    };
+  }, []);
 
   return (
     <>
-      <div className={styles.container + " center"}>
-        <motion.div
-          key="welcome"
-          className={styles.welcome}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 0.7 }}
-          transition={{
-            duration: 20,
-            repeat: Infinity,
-            repeatType: "reverse",
-          }}
-        >
-          Welcome!
-        </motion.div>
-        {colors.map((color, i) => {
-          return (
-            <motion.div
-              key={i}
-              style={{ backgroundColor: color }}
-              className={styles.box}
-              initial={{ opacity: 0.6 }}
-              animate={{ opacity: 0, x: random(100, 50), y: random(100, 50) }}
-              transition={{
-                duration: 20,
-                repeat: Infinity,
-                repeatType: "reverse",
-              }}
-            />
-          );
-        })}
-      </div>
+      <Welcome styles={styles} />
       <section className={styles.information}>
         <h2>Info</h2>
         <div>
