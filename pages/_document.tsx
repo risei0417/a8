55df1b56b5c27cfb48fe82fddf4cbb117c96daeb
risei0417 <daiya5204156@outlook.jp>
2022-09-18 14:38:09 +0900

    #47

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta http-equiv="content-type" content="text/html" />
-        <meta http-equiv="content-language" content="ja" />
+        <meta httpEquiv="content-type" content="text/html" />
+        <meta httpEquiv="content-language" content="ja" />
         <meta name="application-name" content="markone" />
         <meta name="author" content="markone3to1" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
